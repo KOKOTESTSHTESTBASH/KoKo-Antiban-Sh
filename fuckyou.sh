bc0bac83204197aff938d5ce2e339323257354d6
KOKOTESTSHTESTBASH <95129472+KOKOTESTSHTESTBASH@users.noreply.github.com>
2021-11-27 18:43:46 +0630

    Create fuckyou.sh

---
+++ fuckyou.sh
@@ -0,0 +1,49 @@
+ 【a】ahmed【a】
+      
+      【b】ali【b】
+      
+      【c】sumit【c】
+      
+      【d】khan【d】
+      
+       【e】ahmed【e】
+       
+      【f】ali【f】
+      
+      【g】sumit【g】
+      
+      【h】khan【h】
+      
+       【i】ahmed【i】
+       
+      【j】ali【j】
+      
+      【k】sumit【k】
+      
+      【l】khan【l】
+      
+       【m】ahmed【m】
+       
+      【n】ali【n】
+      
+      【o】sumit【o】
+      
+      【p】khan【p】
+      
+        【q】khan【q】
+        
+       【r】ahmed【r】
+       
+      【t】ali【t】
+      
+      【u】sumit【u】
+      
+      【v】khan【v】
+      
+       【w】ahmed【w】
+       
+      【x】ali【x】
+      
+      【y】sumit【y】
+      
+      【z】khan【z】
